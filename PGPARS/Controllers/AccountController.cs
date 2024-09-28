57151d950652294c31830cc2073a118dee985e18
jhart523 <156738122+jhart523@users.noreply.github.com>
2024-09-28 02:24:30 -0700

    started on login form

--- PGPARS/Controllers/AccountController.cs
+++ PGPARS/Controllers/AccountController.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+using PGPARS.Models.ViewModels;
 
 namespace PGPARS.Controllers
 {
     public class AccountController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public IActionResult Login(LoginViewModel loginViewModel)
+        {
+            return View(loginViewModel);
         }
     }
 }
---
+++ PGPARS/Models/ViewModels/LoginViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace PGPARS.Models.ViewModels
+{
+    public class LoginViewModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
