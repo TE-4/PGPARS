5bf2603207708a408c78dcccb133360b152ea41c
jhart523 <156738122+jhart523@users.noreply.github.com>
2024-10-01 22:41:32 -0700

    Login working with default admin credentials. Added authorization in admincontroller so only admin can access

--- PGPARS/Controllers/AdminController.cs
+++ PGPARS/Controllers/AdminController.cs
@@ -1,11 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PGPARS.Data;
 using PGPARS.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace PGPARS.Controllers
 {
+    [Authorize]
     public class AdminController : Controller
     {
+
         public IActionResult Dashboard()
         {
             return View();
