61e4a437a89d64abfbccfd9e35d9ffabc0a7a55e
jhart523 <156738122+jhart523@users.noreply.github.com>
2024-10-14 21:36:06 -0700

    Added RegisterViewModel

---
+++ PGPARS/Models/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PGPARS.Models.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage ="Email is required!")]
+        [EmailAddress(ErrorMessage = "Invalid Email!")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required!")]
+        [UIHint("Password")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage= "Role is required!")]
+        public string Role { get; set; }
+
+
+
+    }
+}
