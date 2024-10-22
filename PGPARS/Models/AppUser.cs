0226ffaf18b983a60dceea78a8bb9d9e60362bbc
jhart523 <156738122+jhart523@users.noreply.github.com>
2024-10-22 02:41:15 -0400

    fixed error in register view

--- PGPARS/Models/AppUser.cs
+++ PGPARS/Models/AppUser.cs
@@ -7,9 +7,9 @@ namespace PGPARS.Models
     public class AppUser : IdentityUser
     {
 
-        [Required]
+       
         public string FirstName { get; set; }
-        [Required]
+        
         public string LastName { get; set; }
 
         public string Position { get; set; }
