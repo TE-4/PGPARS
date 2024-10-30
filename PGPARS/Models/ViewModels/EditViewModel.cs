d16096daf633fb5574e388173350bbe0e330e530
jhart523 <156738122+jhart523@users.noreply.github.com>
2024-10-30 03:09:28 -0400

    edit working now for users

--- PGPARS/Models/ViewModels/EditViewModel.cs
+++ PGPARS/Models/ViewModels/EditViewModel.cs
@@ -18,7 +18,6 @@ namespace PGPARS.Models.ViewModels
         [Display(Name = "N-Number")]
         public string Nnumber { get; set; }
 
-        [Required]
         public string? Position { get; set; }
 
         [Required(ErrorMessage = "Email is required!")]
@@ -31,18 +30,15 @@ namespace PGPARS.Models.ViewModels
 
         // Password Fields
 
-        [Required(ErrorMessage = "Current Password is required!")]
         [DataType(DataType.Password)]
-        public string CurrentPassword { get; set; }
+        public string? CurrentPassword { get; set; }
 
-        [Required(ErrorMessage = "Password is required!")]
         [DataType(DataType.Password)]
-        public string NewPassword { get; set; }
+        public string? NewPassword { get; set; }
 
-        [Required]
         [Compare("NewPassword", ErrorMessage = "The password and confirmation password need to match!")]
         [DataType(DataType.Password)]
-        public string ConfirmPassword { get; set; }
+        public string? ConfirmPassword { get; set; }
 
     }
 }
