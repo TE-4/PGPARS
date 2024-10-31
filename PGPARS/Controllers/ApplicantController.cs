9fc756cf5c5f105946068382c6c1b6970dfaac8b
Tom Espinosa <tomespin4@gmail.com>
2024-10-31 17:24:29 -0400

    ApplicantEdits remade

--- PGPARS/Controllers/ApplicantController.cs
+++ PGPARS/Controllers/ApplicantController.cs
@@ -1,4 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using PGPARS.Models;
+using PGPARS.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Hosting;
 using PGPARS.Data;
 using System.Linq;
@@ -47,5 +55,62 @@ namespace PGPARS.Controllers
 
             return View(applicant);
         }
-    }
+
+        [Authorize(Roles = "Admin")]
+        [HttpGet]
+        public async Task<IActionResult> EditApplicant(string Nnumber)
+        {
+            var applicant = _applicantRepository.GetApplicants().FirstOrDefault(a => a.Nnumber == Nnumber);
+            if (applicant == null)
+            {
+                return NotFound();
+            }
+            var model = new ApplicantEditViewModel
+            {
+                Nnumber = applicant.Nnumber,
+                FirstName = applicant.FirstName,
+                LastName = applicant.LastName,
+                AppSubmitDate = applicant.AppSubmitDate.ToString(),
+                email = applicant.email,
+                Phone = applicant.Phone,
+                AdvisorEmail = applicant.AdvisorEmail,
+                Status = applicant.Status,
+                Race = applicant.Race,
+                Sex = applicant.Sex,
+                Reviewer1 = applicant.Reviewer1,
+                Reviewer2 = applicant.Reviewer2,
+                Rev1Decision = applicant.Rev1Decision,
+                Rev2Decision = applicant.Rev2Decision,
+                RevAgree = applicant.RevAgree.GetValueOrDefault().ToString(),
+                CommitteeReview = applicant.CommitteeReview,
+                Mentor1 = applicant.Mentor1,
+                Mentor2 = applicant.Mentor2,
+                Mentor3 = applicant.Mentor3,
+                SelectMentor = applicant.SelectMentor,
+                GPAOverall = applicant.GPAOverall.ToString(),
+                GPAPsych = applicant.GPAPsych.ToString(),
+                GPAComment = applicant.GPAComment,
+                Course_Req_Met = applicant.Course_Req_Met.GetValueOrDefault().ToString(),
+                CrsReqComment = applicant.CrsReqComment,
+                LetterQuality = applicant.LetterQuality.ToString(),
+                LetterComment = applicant.LetterComment,
+                ResumeQuality = applicant.ResumeQuality.ToString(),
+                ResExpQuality = applicant.ResExpQuality.ToString(),
+                ResumeComment = applicant.ResumeComment,
+                WritSampQuality = applicant.WritSampQuality.ToString(),
+                WritSampComment = applicant.WritSampComment,
+                LORRelevance = applicant.LORRelevance.ToString(),
+                LORQuality = applicant.LORQuality.ToString(),
+                LORComment = applicant.LORComment,
+                OverallFitQuality = applicant.OverallFitQuality.ToString(),
+                OverallFitComments = applicant.OverallFitComments,
+                DecRec = applicant.DecRec,
+                FollowUp = applicant.FollowUp,
+                FinalComments = applicant.FinalComments
+            };
+            return View(model);
+        }
+
+
+    } // END CLASS
 }
