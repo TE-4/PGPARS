0cdd0c93c0e4f57186cd5bc8d26ca7b27cfaab75
Connor Sunman <connorsunman@outlook.com>
2024-10-31 18:09:31 -0400

    Uploading Review page things, WIP

---
+++ PGPARS/Data/FakeReviewRepository.cs
@@ -0,0 +1,17 @@
+﻿using PGPARS.Models;
+using System.Collections.Generic;
+namespace PGPARS.Data
+{
+    public class FakeReviewRepository : IReviewRepository
+    {
+        public IEnumerable<Review> GetReviews()
+        {
+            return new List<Review>
+            {
+                new Review { ReviewId = "Smith", IsRejected = false },
+                new Review { ReviewId = "Chris", IsRejected = true },
+                new Review { ReviewId = "Andy", IsRejected = false },
+            };
+        }
+    }
+}
