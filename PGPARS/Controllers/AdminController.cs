8ed72eef2ca7cd362fd7ebdd9d1f65f18b081ee1
Tom Espinosa <tomespin4@gmail.com>
2024-12-03 21:02:51 -0500

    Super speed! (Background removed)

--- PGPARS/Controllers/AdminController.cs
+++ PGPARS/Controllers/AdminController.cs
@@ -4,13 +4,23 @@ using PGPARS.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.Data;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
 
 namespace PGPARS.Controllers
 {
     [Authorize]
     public class AdminController : Controller
     {
-        
+        private readonly IApplicantRepository _applicantRepository;
+        private readonly IFundingRepository _fundingRepository;
+        private readonly IReviewRepository _reviewRepository;
+
+        public AdminController()
+        {
+            //_applicantRepository = applicantRepository;
+
+        }
+
         public IActionResult Dashboard()
         {
             if (User.Identity.IsAuthenticated)
