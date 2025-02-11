eb6b116494168be36d78125fe117c25788235a82
jhart523 <156738122+jhart523@users.noreply.github.com>
2025-02-11 04:10:15 -0500

    added category filter to action history

--- PGPARS/Controllers/AuditController.cs
+++ PGPARS/Controllers/AuditController.cs
@@ -12,8 +12,13 @@ namespace PGPARS.Controllers
             _auditRepository = auditRepo;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(string filter)
         {
+            if(filter != null)
+            {
+                var filteredLogs = _auditRepository.GetLogs().Where(log => log.Category == filter);
+                return View(filteredLogs);
+            }
             var logs = _auditRepository.GetLogs();
             return View(logs);
         }
