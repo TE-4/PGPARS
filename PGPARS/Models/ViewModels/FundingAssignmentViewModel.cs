f043eb474d76f796666b48e6b15287febdb8e51e
Alex Lovera <n01492488@unf.edu>
2025-02-12 15:59:14 -0500

    declared Applicants as nullable

--- PGPARS/Models/ViewModels/FundingAssignmentViewModel.cs
+++ PGPARS/Models/ViewModels/FundingAssignmentViewModel.cs
@@ -2,11 +2,10 @@
 {
     public class FundingAssignmentViewModel
     {
-        private IEnumerable<Applicant> applicants;
 
         public int FundingSourceId { get; set; }
         public string? FundingSourceName { get; set; }
-        public IEnumerable<Applicant> Applicants { get => applicants; set => applicants = value; }
+        public IEnumerable<Applicant>? Applicants { get; set; }
         public decimal RemainingAmount { get; set; }
         public string? ApplicantId { get; set; } // Ensure this is a string to match the Nnumber type
         public decimal Amount { get; set; }
