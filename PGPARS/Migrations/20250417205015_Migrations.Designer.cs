b274483d9ad427f8de1cabba7bad581f2ae8db12
Tom Espinosa <tomespin4@gmail.com>
2025-04-17 17:51:51 -0400

    Funding Allocations & Directory style updates

--- PGPARS/Migrations/20250417205015_Migrations.Designer.cs
+++ PGPARS/Migrations/20250417205015_Migrations.Designer.cs
@@ -12,8 +12,8 @@ using PGPARS.Data;
 namespace PGPARS.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20250417014721_first")]
-    partial class first
+    [Migration("20250417205015_Migrations")]
+    partial class Migrations
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
--- PGPARS/Migrations/20250417205015_Migrations.cs
+++ PGPARS/Migrations/20250417205015_Migrations.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace PGPARS.Migrations
 {
     /// <inheritdoc />
-    public partial class first : Migration
+    public partial class Migrations : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
